a0eb4f4eaa294d4d6502909fc22690de920b66fe
Jake <56329025+jakeo-dev@users.noreply.github.com>
2023-08-27 18:19:38 -0700

    Better customizability

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+maxValue = int(input(
+    'Maximum possible value for an extracurricular: '))
 ecListInput = input(
-    'Comma-separated list of values from 1 to 3 representing the strength of each extracurricular: ')
+    'Comma-separated list of values from 1 to ' + str(maxValue) + ' representing the strength of each extracurricular: ')
 
 ecList = list(ecListInput.split(','))
 
@@ -9,8 +11,8 @@ for e in ecList:
 
 avg = sum / len(ecList)
 
-# standardizes the average (3 because the ranking is from 1 to 3)
-stand = avg / 3
+# standardizes the average (maxValue because the ranking is from 1 to maxValue)
+stand = avg / maxValue
 
 # rewards doing more activities
 final = stand * len(ecList)
